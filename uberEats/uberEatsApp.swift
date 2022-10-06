04b763575e913b5eb64f3fbb9dfab7f2b9c30f6a
marvin evins <marvinevins@yahoo.com>
2022-10-06 09:56:37 -0500

    Initial Commit

---
+++ uberEats/uberEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberEatsApp.swift
+//  uberEats
+//
+//  Created by marvin evins on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct uberEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
