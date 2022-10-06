13d850cd3b163c3e870185538e230814cb5b0e41
marvin evins <marvinevins@yahoo.com>
2022-10-06 13:32:53 -0500

    working on this

--- uberEats/Core/Views/UberMapViewRepresentable.swift
+++ uberEats/Core/Views/UberMapViewRepresentable.swift
@@ -11,8 +11,9 @@ import MapKit
 struct UberMapViewRepresentable: UIViewRepresentable {
     
     let mapView = MKMapView()
-    
+    let locationManager = LocationManager()
     func makeUIView(context: Context) -> some  UIView{
+        mapView.delegate = context.coordinator
         mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
@@ -21,7 +22,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView (_ uiView: UIViewType, context: Context){
-        
+        //needs to be updated
     }
     func makeCoordinator() -> MapCoordinator{
         return  MapCoordinator(parent: self)
@@ -36,5 +37,11 @@ extension UberMapViewRepresentable  {
             self.parent = parent
             super.init()
         }
+        
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            parent.mapView.setRegion(region, animated: true)
+        }
+        
     }
 }
