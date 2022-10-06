dab36d9b3f7179f6a026bba30667e63e53aa341c
marvin evins <marvinevins@yahoo.com>
2022-10-06 10:03:55 -0500

    working on app set up.. folders miscellaneous

--- uberEats/App/uberEatsApp.swift
+++ uberEats/App/uberEatsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct uberEatsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ uberEats/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  uberEats
+//
+//  Created by marvin evins on 10/6/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
